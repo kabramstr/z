6309e3bce15f104300c7b3734258f3feeec9331e
kabramstr <107558@lingecollege.nl>
2025-05-09 09:07:38 +0200

    Create new Vite project with React and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-gray-900">Welcome to My App</h1>
+    </div>
+  )
+}
+
+export default App
